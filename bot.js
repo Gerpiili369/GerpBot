7c6cac3a2d9c05045591714c31571e5e2202bbbe
Gerpiili369 <elektromiikka@hotmail.com>
2017-12-08 19:05:46 +0200

    start

---
+++ bot.js
@@ -0,0 +1,37 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+var bot = new Discord.Client({
+    token: auth.token,
+    autorun: true
+});
+
+bot.on('ready', evt => {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', (user, userID, channelID, message, evt) => {
+    if (message.substring(0, 3) == 'gb!') {
+        var args = message.substring(4).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch (cmd) {
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+                break;
+        }
+    }
+});
