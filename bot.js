9d964bdeb9e8938914882828b77cd40f1de7e9b1
Gerpiili369 <elektromiikka@hotmail.com>
2018-02-08 13:58:29 +0200

    saving empty setting now blocked

--- bot.js
+++ bot.js
@@ -264,5 +264,5 @@ function getJSON(file,location = '') {
 }
 
 function updateSettings() {
-    fs.writeFile('settings.json', JSON.stringify(settings), err => {if (err) logger.error(err)});
+    if (JSON.stringify(settings) != '') fs.writeFile('settings.json', JSON.stringify(settings), err => {if (err) logger.error(err)});
 }
