643baed1ce01a8217878d2fdb95e3657335981d8
Gerpiili369 <elektromiikka@hotmail.com>
2018-09-06 22:18:08 +0300

    changed channel and server to channelID and serverID

--- scripts/permCheck.js
+++ scripts/permCheck.js
@@ -1,61 +1,61 @@
 const permDic = require('discord.io').Permissions;
 
 module.exports = bot => ({
-    userHasPerm: function (server, user = bot.id, perm = 'admin', channel = '') {
+    userHasPerm: function (serverID, user = bot.id, perm = 'admin', channelID = '') {
         // Owner override
-        if (user == bot.servers[server].owner_id) return true;
+        if (user == bot.servers[serverID].owner_id) return true;
 
         const permdex = permDic[perm];
         let hasPerm = false;
         // Server everyone permissions
-        if (bot.servers[server].roles[server]._permissions.toString(2).split('').reverse()[permdex] == 1) hasPerm = true;
+        if (bot.servers[serverID].roles[serverID]._permissions.toString(2).split('').reverse()[permdex] == 1) hasPerm = true;
 
         // Server role permissions
-        for (const role of bot.servers[server].members[user].roles) {
+        for (const role of bot.servers[serverID].members[user].roles) {
             // Admin override
-            if (bot.servers[server].roles[role]._permissions.toString(2).split('').reverse()[3] == 1) return true;
-            if (bot.servers[server].roles[role]._permissions.toString(2).split('').reverse()[permdex] == 1) hasPerm = true;
+            if (bot.servers[serverID].roles[role]._permissions.toString(2).split('').reverse()[3] == 1) return true;
+            if (bot.servers[serverID].roles[role]._permissions.toString(2).split('').reverse()[permdex] == 1) hasPerm = true;
         }
 
-        if (bot.channels[channel]) {
-            if (bot.channels[channel].permissions.role[server]) {
+        if (bot.channels[channelID]) {
+            if (bot.channels[channelID].permissions.role[serverID]) {
                 // Channel everyone deny
-                if (bot.channels[channel].permissions.role[server].deny.toString(2).split('').reverse()[permdex] == 1) hasPerm = false;
+                if (bot.channels[channelID].permissions.role[serverID].deny.toString(2).split('').reverse()[permdex] == 1) hasPerm = false;
                 // Channel everyone allow
-                if (bot.channels[channel].permissions.role[server].allow.toString(2).split('').reverse()[permdex] == 1) hasPerm = true;
+                if (bot.channels[channelID].permissions.role[serverID].allow.toString(2).split('').reverse()[permdex] == 1) hasPerm = true;
             }
 
-            for (const role in bot.channels[channel].permissions.role) {
-                if (bot.servers[server].members[user].roles.indexOf(role) != -1) {
+            for (const role in bot.channels[channelID].permissions.role) {
+                if (bot.servers[serverID].members[user].roles.indexOf(role) != -1) {
                     // Channel role deny
-                    if (bot.channels[channel].permissions.role[role].deny.toString(2).split('').reverse()[permdex] == 1) {
+                    if (bot.channels[channelID].permissions.role[role].deny.toString(2).split('').reverse()[permdex] == 1) {
                         hasPerm = false;
                     }
                     // Channel role allow
-                    if (bot.channels[channel].permissions.role[role].allow.toString(2).split('').reverse()[permdex] == 1) {
+                    if (bot.channels[channelID].permissions.role[role].allow.toString(2).split('').reverse()[permdex] == 1) {
                         hasPerm = true;
                         break;
                     }
                 }
             }
 
-            if (bot.channels[channel].permissions.user[user]) {
+            if (bot.channels[channelID].permissions.user[user]) {
                 // Channel user deny
-                if (bot.channels[channel].permissions.user[user].allow.toString(2).split('').reverse()[permdex] == 1) hasPerm = true;
+                if (bot.channels[channelID].permissions.user[user].allow.toString(2).split('').reverse()[permdex] == 1) hasPerm = true;
                 // Channel user allow
-                if (bot.channels[channel].permissions.user[user].deny.toString(2).split('').reverse()[permdex] == 1) hasPerm = false;
+                if (bot.channels[channelID].permissions.user[user].deny.toString(2).split('').reverse()[permdex] == 1) hasPerm = false;
             }
         }
 
         return hasPerm;
     },
-    missage: function (msg, channel, perms = []) {
-        if (this.userHasPerm(bot.channels[channel].guild_id, bot.id, 'TEXT_EMBED_LINKS', channel)) {
-            msg(channel, '', {
+    missage: function (msg, channelID, perms = []) {
+        if (this.userHasPerm(bot.channels[channelID].guild_id, bot.id, 'TEXT_EMBED_LINKS', channelID)) {
+            msg(channelID, '', {
                 title: 'Following permissions required:',
                 description: '`' + perms.join('\n') + '`',
                 color: 16711680
             });
-        } else msg(channel, 'Following permissions required:\n`' + perms.join('\n') + '`');
+        } else msg(channelID, 'Following permissions required:\n`' + perms.join('\n') + '`');
     }
 });
