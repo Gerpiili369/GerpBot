612b5be691346d9796a9f63086c7e08a2cc397a0
Gerpiili369 <elektromiikka@hotmail.com>
2018-11-10 12:59:36 +0200

    made .osr pro file encoder into its own thin

---
+++ scripts/fileCoder.js
@@ -0,0 +1,135 @@
+module.exports = class ProFileCoder {
+    constructor() {
+        this.byteArray = [];
+        this.dataArray = [];
+        this.byteIndex = 0;
+        this.types = {
+            byte: 1,
+            short: 2,
+            int: 4,
+            long: 8
+        }
+    }
+
+    fromBuffer(buffer) {
+        if (buffer instanceof Buffer)
+            this.byteArray = this.hexStringToArray(buffer.toString('hex'));
+        return this;
+    }
+
+    toBuffer() {
+        return new Buffer(this.byteArray.join(''), 'hex');
+    }
+
+    addFromBuffer(buffer) {
+        if (buffer instanceof Buffer) {
+            this.byteArray.push(...this.hexStringToArray(buffer.toString('hex')));
+        }
+        return this;
+    }
+
+    addString(data) {
+        if (data) {
+            const
+                utfArray = this.hexStringToArray(Buffer.from(data, 'utf-8').toString('hex')),
+                length = utfArray.length,
+                bitData = length.toString(2),
+                lebArray = [];
+
+            for (let i = bitData.length; i > 0; i -= 7) lebArray.push(
+                parseInt(
+                    (i - 7 > 0 ? '1' : '0') + bitData.substring(i - 7, i), 2
+                ).toString(16).padStart(2, '0')
+            )
+
+            this.byteArray.push('0b', ...lebArray, ...utfArray)
+        } else this.byteArray.push('00');
+        return this;
+    }
+
+    addValue(data, type = 'int') {
+        const
+            size = this.types[type],
+            bitData = Number(data).toString(2),
+            byteArray = [];
+
+        for (let i = bitData.length; i > 0; i -= 8) {
+            let hexByte = parseInt(bitData.substring(i - 8, i), 2).toString(16)
+            if (i - 8 > 0);
+            byteArray.push(hexByte.padStart(2, '0'))
+        }
+
+        while(byteArray.length < size) byteArray.push('00');
+
+        this.byteArray.push(...byteArray);
+        return this;
+    }
+
+    decodeString(name = '') {
+        switch(this.byteArray[this.byteIndex]) {
+            case '00':
+                this.byteIndex++
+                this.dataArray.push({
+                    name,
+                    value: '',
+                    type: 'string',
+                });
+                break;
+            case '0b':
+                this.byteIndex++
+
+                const
+                    byteArray = [],
+                    utfArray = [],
+                    lebArray = []
+
+                for (let i = this.byteIndex; true; i++) {
+                    let group = parseInt(this.byteArray[i], 16).toString(2);
+                    if (group.length < 8) {
+                        lebArray.push(group);
+                        break;
+                    } else lebArray.push(group.substring(1));
+                }
+                this.byteIndex += lebArray.length;
+
+                for (let i = this.byteIndex; i < this.byteIndex + Number(parseInt(lebArray.reverse().join(), 2).toString(10)); i++) {
+                    utfArray.push(this.byteArray[i])
+                }
+
+                this.byteIndex += utfArray.length;
+                this.dataArray.push({
+                    name,
+                    value: Buffer.from(utfArray.join(''), 'hex').toString('utf8'),
+                    type: 'string',
+                });
+                break;
+        }
+
+        return this;
+    }
+
+    decodeValue(name = '', type = 'int') {
+        const
+            size = this.types[type],
+            byteArray = [];
+
+        for (let i = this.byteIndex; i < this.byteIndex + size; i++) {
+            byteArray.push(this.byteArray[i])
+        }
+
+        this.byteIndex += size;
+        this.dataArray.push({
+            name,
+            value: parseInt(byteArray.reverse().join(''), 16).toString(10),
+            type,
+        });
+        return this;
+    }
+
+    hexStringToArray(string) {
+        const array = [];
+        for (let i = string.length; i > 0; i -= 2)
+            array.push(string.substring(i - 2, i).padStart(2, '0'));
+        return array.reverse();
+    }
+}
--- scripts/osuBestReplay.js
+++ scripts/osuBestReplay.js
@@ -1,3 +1,5 @@
+const FileCoder = require('./fileCoder.js');
+
 module.exports = osu => (username, playNumber = 1) => new Promise((resolve, reject) => {
     let play, mapHash;
 
@@ -14,144 +16,31 @@ module.exports = osu => (username, playNumber = 1) => new Promise((resolve, reje
 
             const
                 cdr = Buffer.from(replayData.content, replayData.encoding),
-                file = new ProEncode, mode = 0;
+                file = new FileCoder, mode = 0;
 
-            file.byte(mode)
-                .int(3162190593) // version placeholder
-                .string(mapHash)
-                .string(username)
-                .string('') // replay hash
-                .short(play.count300)
-                .short(play.count100)
-                .short(play.count50)
-                .short(play.countgeki)
-                .short(play.countkatu)
-                .short(play.countmiss)
-                .int(play.score)
-                .short(play.maxcombo)
-                .byte(play.perfect)
-                .int(play.enabled_mods)
-                .string('') // ife bar graph
-                .long(new Date(play.date.replace(' ', 'T')).getTime() * 10000)
-                .int(cdr.length)
-                .add(cdr)
-                .long(0); // unknown
+            file.addValue(mode, 'byte')
+                .addValue(3162190593, 'int') // version placeholder
+                .addString(mapHash)
+                .addString(username)
+                .addString('') // replay hash
+                .addValue(play.count300, 'short')
+                .addValue(play.count100, 'short')
+                .addValue(play.count50, 'short')
+                .addValue(play.countgeki, 'short')
+                .addValue(play.countkatu, 'short')
+                .addValue(play.countmiss, 'short')
+                .addValue(play.score, 'int')
+                .addValue(play.maxcombo, 'short')
+                .addValue(play.perfect, 'byte')
+                .addValue(play.enabled_mods, 'int')
+                .addString('') // ife bar graph
+                .addValue(new Date(play.date.replace(' ', 'T')).getTime() * 10000, 'long')
+                .addValue(cdr.length, 'int')
+                .addFromBuffer(cdr)
+                .addValue(0, 'long'); // unknown
 
             resolve(file);
         })
         .catch(reject);
 
 });
-
-class ProEncode {
-    constructor() {
-        this.byteArray = [];
-        this.lastType = '';
-        this.insideAdd = false;
-        this.typeLengths = {
-            byte: 1,
-            short: 2,
-            int: 4,
-            long: 8
-        }
-    }
-
-    add(bytes) {
-        let byteArray = [], l = this.byteArray.length
-
-        if (bytes instanceof Buffer)
-            byteArray = this.hexStringToArray(bytes.toString('hex'));
-        else
-            byteArray = bytes instanceof Array ? bytes : [bytes];
-
-        this.byteArray = this.byteArray.concat(byteArray);
-
-        if (!this.insideAdd) {
-            this.lastType = 'other';
-            this.insideAdd = false;
-        }
-
-        return this;
-    }
-
-    toBuffer() {
-        return new Buffer(this.byteArray.join(''), 'hex');
-    }
-
-    byte(data) {
-        this.byteAdd(data, 1)
-        this.lastType = 'byte';
-        this.insideAdd = true;
-        return this;
-    }
-
-    short(data) {
-        this.byteAdd(data, 2)
-        this.lastType = 'short';
-        this.insideAdd = true;
-        return this;
-    }
-
-    int(data) {
-        this.byteAdd(data, 4)
-        this.lastType = 'int';
-        this.insideAdd = true;
-        return this;
-    }
-
-    long(data) {
-        this.byteAdd(data, 8)
-        this.lastType = 'long';
-        this.insideAdd = true;
-        return this;
-    }
-
-    string(data) {
-        if (data) {
-            const
-                utfArray = this.hexStringToArray(Buffer.from(data, 'utf-8').toString('hex')),
-                length = utfArray.length,
-                bitData = length.toString(2),
-                lebArray = [];
-
-            for (let i = bitData.length; i > 0; i -= 7) lebArray.push(
-                parseInt(
-                    (i - 7 > 0 ? '1' : '0') + bitData.substring(i - 7, i), 2
-                ).toString(16).padStart(2, '0')
-            )
-
-            this.add(['0b', ...lebArray, ...utfArray]);
-        } else this.add('00');
-
-        this.lastType = 'string';
-        this.insideAdd = true;
-
-        return this;
-    }
-
-    hexStringToArray(string) {
-        const array = [];
-        for (let i = string.length; i > 0; i -= 2) {
-            array.push(string.substring(i - 2, i).padStart(2, '0'))
-        }
-        return array.reverse();
-    }
-
-    byteAdd(data, amt = 1) {
-        if (typeof data === 'string') data = Number(data);
-
-        const
-            bitData = data.toString(2),
-            byteArray = [];
-
-        for (let i = bitData.length; i > 0; i -= 8) {
-            let hexByte = parseInt(bitData.substring(i - 8, i), 2).toString(16)
-            if (i - 8 > 0);
-            byteArray.push(hexByte.padStart(2, '0'))
-        }
-
-        while(byteArray.length < amt) byteArray.push('00');
-
-        this.add(byteArray);
-    }
-}
