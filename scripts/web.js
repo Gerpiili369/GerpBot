61d37f16fcaa751f2ac35b69eda8e098a0c7175c
Gerpiili369 <elektromiikka@hotmail.com>
2019-06-03 22:39:27 +0300

    Web path improvements

--- scripts/web.js
+++ scripts/web.js
@@ -1,5 +1,5 @@
 const
-    config = require('../config').web || {},
+    config = require('../config'),
     path = require('path'),
     express = require('express'),
     app = express(),
@@ -7,14 +7,20 @@ const
     fs = require('fs'),
     tempath = path.join(__dirname, '..', 'temp');
 
-!fs.existsSync(tempath) && fs.mkdirSync(tempath);
+if (!config.web) config.web = {
+    root: '',
+};
 
 module.exports = {
-    activate: new Promise(resolve => {
-        app.use(config.root + '/temp', express.static(tempath));
-        http.listen(config.port, config.host, () => {
-            resolve('Activated http-service.');
-        });
+    activate: new Promise((resolve, reject) => {
+        checkTempath()
+            .then(() => {
+                app.use(path.join(config.web.root, 'temp'), express.static(tempath));
+                http.listen(config.web.port, config.web.host, () => {
+                    resolve('Activated http-service.');
+                });
+            })
+            .catch(reject);
     }),
     addTemp: (name, buffer) => new Promise((resolve, reject) => {
         fs.writeFile(path.join(tempath, name), buffer, err => {
@@ -28,4 +34,16 @@ module.exports = {
             else resolve();
         });
     })
+};
+
+/**
+ * @returns {Promise}
+ */
+function checkTempath() {
+    return new Promise((resolve, reject) => {
+        fs.access(tempath, fs.constants.F_OK, err => {
+            if (err) fs.mkdir(tempath, err => err ? reject(err) : resolve());
+            else resolve();
+        });
+    });
 }
